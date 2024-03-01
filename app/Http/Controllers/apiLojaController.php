e20fb2cfe39e68d9704d45084badaccd7b6aa937
pedro-aranha <pedro.aranha@outlook.com>
2024-03-01 15:27:05 -0300

    New routes
    New routes and better return messages

--- app/Http/Controllers/apiLojaController.php
+++ app/Http/Controllers/apiLojaController.php
@@ -18,7 +18,39 @@ class apiLojaController extends Controller
             "description" => $req->description
         ]);
 
-        return response(["Ok", 200]);
+        return response(["Information stored"], 200);
     }
+
+    public function update(Request $req){
+       $product = apiLoja::find($req->id);
+        $return = "Information -";
+        if($product->name != $req->name){
+            $product->name = $req->name;
+            $return .=" Name "; 
+        }
+        
+        if($product->price != $req->price){
+            $product->price = $req->price;
+            $return .=" Price "; 
+        }
+        if($product->description != $req->description){
+            $product->description = $req->description;
+            $return .=" Description "; 
+        }
+        $product->save();
+        $return .= "successfully changed ";
+        return response([$return], 200);
+    }
+
+    public function delete(Request $req){
+        $product = apiLoja::find($req->id);
+            if($product){
+                $product->delete();
+                return response(["Product Deleted"], 200);
+            }else{
+                return response(["Product not found"], 200);
+            }
+        
+     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,17 @@
 <?php
 
 use App\Http\Controllers\apiLojaController;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 //index
 Route::get('apiLoja',[apiLojaController::class,"index"]);
 
 //store
 Route::post('apiLoja',[apiLojaController::class,"store"]);
+
+//Update
+Route::put('apiLoja/{id}',[apiLojaController::class,"update"]);
+
+//Delete
+Route::delete('apiLoja/{id}',[apiLojaController::class,"delete"]);
