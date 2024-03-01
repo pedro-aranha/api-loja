710239627d80d36ea1a9d7ebb6dee93a980cbc90
pedro-aranha <pedro.aranha@outlook.com>
2024-03-01 14:12:00 -0300

    Initial commit

---
+++ app/Models/apiLoja.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class apiLoja extends Model
+{
+    use HasFactory;
+}
